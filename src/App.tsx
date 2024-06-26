8a3c571ad2b9fefc1724841f8f468fda4ace6ead
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-06-26 16:57:12 +0530

    Implemented basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Login from "./components/Login";
 import MapComponent from "./components/MapComponent";
 
 const App = () => {
   return (
     <div className="w-[100%] h-[100%]">
-      {/* <MapComponent /> */}
-      <Login />
+      <MapComponent />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./components/Login.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
