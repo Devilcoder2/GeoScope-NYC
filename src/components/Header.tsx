bee9fa25ce66c2e8e80deb2f56c8c4c839f7c0a6
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-06-28 16:44:37 +0530

    Implemented the functionality of downloading the map

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,19 @@ const Header = () => {
         <h1 className="text-2xl ml-10 font-semibold text-white ">MapSense</h1>
       </div>
 
-      <div className="">
+      <div className="flex justify-center items-center">
+        <div>
+          <a
+            id="export-png"
+            className="bg-[#3590F0] border-2 border-white text-white mr-10 py-2  px-3 rounded-full "
+            role="button"
+          >
+            <i className="fa fa-download"></i>{" "}
+            <h1 className="hidden lg:inline">Download as PNG</h1>
+          </a>
+          <a id="image-download" download="map.png"></a>
+        </div>
+
         <button
           className="bg-[#3590F0] border-2 border-white text-white mr-10 py-1  px-3 rounded-full "
           onClick={logoutHandler}
--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -281,6 +281,78 @@ const MapComponent: React.FC = () => {
         map.on("pointermove", handleHover);
       }
 
+      //Download
+      document.getElementById("export-png")?.addEventListener("click", () => {
+        map.once("rendercomplete", () => {
+          const mapCanvas = document.createElement("canvas");
+          const size = map.getSize();
+          if (size !== undefined) {
+            mapCanvas.width = size[0];
+            mapCanvas.height = size[1];
+          }
+          const mapContext = mapCanvas.getContext("2d");
+          Array.prototype.forEach.call(
+            map
+              .getViewport()
+              .querySelectorAll(".ol-layer canvas, canvas.ol-layer"),
+            function (canvas) {
+              if (canvas.width > 0) {
+                const opacity =
+                  canvas.parentNode.style.opacity || canvas.style.opacity;
+
+                if (mapContext !== null)
+                  mapContext.globalAlpha = opacity === "" ? 1 : Number(opacity);
+                let matrix;
+                const transform = canvas.style.transform;
+                if (transform) {
+                  // Get the transform parameters from the style's transform matrix
+                  matrix = transform
+                    .match(/^matrix\(([^()]*)\)$/)[1]
+                    .split(",")
+                    .map(Number);
+                } else {
+                  matrix = [
+                    parseFloat(canvas.style.width) / canvas.width,
+                    0,
+                    0,
+                    parseFloat(canvas.style.height) / canvas.height,
+                    0,
+                    0,
+                  ];
+                }
+                // Apply the transform to the export map context
+                CanvasRenderingContext2D.prototype.setTransform.apply(
+                  mapContext,
+                  matrix
+                );
+                const backgroundColor = canvas.parentNode.style.backgroundColor;
+                if (mapContext !== null) {
+                  if (backgroundColor) {
+                    mapContext.fillStyle = backgroundColor;
+                    mapContext.fillRect(0, 0, canvas.width, canvas.height);
+                  }
+                  mapContext.drawImage(canvas, 0, 0);
+                }
+              }
+            }
+          );
+
+          if (mapContext !== null) {
+            mapContext.globalAlpha = 1;
+            mapContext.setTransform(1, 0, 0, 1, 0, 0);
+          }
+
+          const link = document.getElementById(
+            "image-download"
+          ) as HTMLAnchorElement;
+          if (link !== null) {
+            link.href = mapCanvas.toDataURL();
+            link.click();
+          }
+        });
+        map.renderSync();
+      });
+
       return () => {
         map.un("pointermove", handleHover);
         map.setTarget(undefined);
